0261a0717bd6bce26bcb73fd7ba6c772fdbf0650
Jim Browne <jbrowne@jbrowne.com>
2013-12-27 13:36:46 -0800

    Initial open sourcing

---
+++ README.md
@@ -0,0 +1,140 @@
+Tools for working with AWS temporary and multi-factor authentication credentials
+--------------------------------------------------------------------------------
+
+When using an IAM user that has MFA enabled, temporary credentials are issued
+using the [GetSessionToken](http://docs.aws.amazon.com/STS/latest/APIReference/API_GetSessionToken.html)
+API call.  We've written a tool to make the GetSessionToken call and manage environment variables
+to make working with temporary credentials more convenient.  We've also included a tool that scripts
+can use to receive temporary credentials from an [AssumeRole](http://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRole.html)
+call.
+
+Requirements
+------------
+
+[Boto](https://github.com/boto/boto) version 2.21.1 or greater
+
+Ideally boto with the following [patch](https://github.com/boto/boto/pull/1942) (not yet merged)
+
+Credential directory
+--------------------
+
+The tools assume you have a credential directory in ```{$HOME}/aws-creds```:
+
+    jbrowne@foo:~$ ls -l ~/aws-creds/ | grep -v .cache
+    work.access_key
+    work.secret_key
+    personal.access_key
+    personal.secret_key
+
+These are IAM role user keys that have no permissions on their own; rather they are 
+nly useful once one has authenticated with MFA.  This is enforced by including:
+
+    "Condition":{
+            "Null":{"aws:MultiFactorAuthAge":"false"}
+    }
+
+in the IAM permissions.
+
+The credential directory is also used to cache credentials received from GetSessionToken.
+
+Account file
+------------
+
+The tools assume there is a JSON file with account information.  The location can
+be overridden by passing ```--config```.  Example file:
+
+    {
+        "personal": "arn:aws:iam::987654321012:",
+        "work":  "arn:aws:iam::012123456789:"
+    }
+
+The numbers are the account IDs.  These can be found on the account summary page.
+
+Setting credentials
+-------------------
+"Setting credentials" means to set the appropriate environment variables, specifically:
+
+* ec2 cli environment variables                                             
+
+    AWS_ACCESS_KEY
+    AWS_SECRET_KEY
+    AWS_DELEGATION_TOKEN
+
+* boto and aws-cli environment variables                                    
+
+    AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY
+    AWS_SECURITY_TOKEN (not yet suported for boto, see Requirements section)
+
+*Note*: boto doesn't yet support AWS_SECURITY_TOKEN
+
+Ensure ```ec2switch.sh``` is in ```/etc/profile.d``` or:
+
+   source ec2switch.sh
+
+
+1. To start, no keys are set:
+
+    jbrowne@foo:~$ ec2now
+    No keys set
+
+2. Let's see what accounts are available:
+
+    jbrowne@foo:~$ ec2switch 
+    Possible accounts: work personal 
+
+3. Let's switch to the IAM credentials for ```${USER}```, account personal
+
+    jbrowne@foo:~$ ec2switch personal
+    Cache invalid or not present for account personal
+    Set keys for personal AWS account
+
+4. We can see that the credentials are set:
+
+    jbrowne@foo:~$ ec2now
+    Keys set for personal AWS account
+    Region is us-east-1
+
+5. Let's escalate to MFA confirmed credentials:
+
+    jbrowne@foo:~$ ec2mfa
+    Enter token for user jbrowne: 281823
+    Cache is valid for account personal
+    Set keys for personal-MFA-STS AWS account
+
+6. ```ec2now``` reflects the presence of MFA credentials:
+
+    jbrowne@foo:~$ ec2now
+    Keys set for personal-MFA-STS AWS account
+    Region is us-east-1
+
+7. Switch to the work account, no cache present, so no MFA creds:
+
+    jbrowne@foo:~$ ec2switch work
+    Cache not present for account work
+    Set keys for work AWS account
+
+8. Switch back to the personal account, MFA credentials are cached:
+
+    jbrowne@foo:~$ ec2switch personal
+    Cache is valid for account personal
+    Set keys for personal-MFA-STS AWS account
+
+Generating AssumeRole temporary credentials for scripts
+-------------------------------------------------------
+
+```generate-sts-role-token``` calls AssumeRole to generate temporary authentication
+credentials.  The default is to emit JSON (for use by other scripts), but it can also
+emit copy/paste-able SHELL environment variable statements.
+
+*Note*: Currently --token has to be passed as boto does not (yet) support the
+AWS_SECURITY_TOKEN environment variable.
+
+
+    jbrowne@foo:~$ generate-sts-role-token --account work --role work-temporary-escalation --token ${AWS_SECURITY_TOKEN} 
+    {
+        "access_key": "ASIAABCDEFGHIJK3ZP5A",
+        "expiration": "2013-12-20T00:59:32Z",
+        "secret_key": "kl0y+01234567897cmAzxABCDEFGHIJ6a7pTXQde",
+        "session_token": "AQoDYXdzREALLYLONGTOKENHERE=="
+    }
---
+++ ec2switch.sh
@@ -0,0 +1,207 @@
+# Copyright 2013 42Lines, Inc.
+# Original Author: Jim Browne
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+####
+# Set AWS-related environment variables
+# Globals: none
+# Arguments: $1 account; $2 accessKey; $3 secretAccessKey; $4 (optional) token
+# Returns: none
+####
+function _ec2setvars()
+{
+    # Dropped support for credential file; anything other than boto using it?
+    #export AWS_CREDENTIAL_FILE=${basedir}/aws.credentials.${account}
+    
+    # ec2 cli environment variables
+    export AWS_ACCESS_KEY=$2
+    export AWS_SECRET_KEY=$3
+    
+    # boto and aws-cli environment variables
+    export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
+    export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
+    
+    if [[ -n "$4" ]]; then
+	# aws-cli environment variable
+	export AWS_SECURITY_TOKEN=$4
+	# ec2 cli environment variable
+	export AWS_DELEGATION_TOKEN=$4
+    else
+	unset AWS_SECURITY_TOKEN
+	unset AWS_DELEGATION_TOKEN
+    fi
+}
+
+####
+# Check if local files caching AWS STS credntials are present and not expired
+# Globals: none
+# Arguments: $1 basedir; $2 account
+# Returns: true if the cache is valid and current; false if not
+####
+function _ec2checkcache()
+{
+    local filename=.cache.$2.expiration
+    local file=$1/${filename}
+    local token=$1/.cache.$2.session_token
+
+    if [[ ! -f "${file}" ]]; then
+	echo "Cache not present for account ${2}"
+	return 1
+    fi
+
+    # The expiration file contains an integer specifying the number of
+    # minutes the temporary credential is valid
+    local expiration
+    expiration=$(cat ${file})
+
+    # See if the duration since the modification time (when the
+    # credential was recorded) is greater than the duration (lifetime)
+    # of the temporary credential
+    if [[ $(find "$1" -name "${filename}" -mmin -"${expiration}" \
+	| wc -l) -gt 0 ]]; then
+	echo "Cache is valid for account ${2}"
+	return 0
+    else
+	# If the cache is expired, remove the session_token file
+	[[ -f "${token}" ]] && rm -f "${token}"
+	echo "Cache invalid or not present for account ${2}"
+	return 1
+    fi
+}
+
+####
+# Set AWS-related environment variables based on account selected and possible
+# presence of cached STS credentials
+#
+# Globals: none
+# Arguments: $1 desired account
+# Returns: none
+####
+function ec2switch()
+{
+
+    local basedir=${HOME}/aws-creds
+
+    if [[ ! -d "${basedir}" ]]; then
+	echo "No creds directory? ${basedir}"
+    elif [[ -z "$1" ]]; then
+	echo -n "Possible accounts: "
+	ls ${basedir}/*.access_key \
+	    | sed -e 's/.*aws-creds\/\(.*\).access_key/\1/' \
+	    | xargs -I{} echo -n "{} "
+	echo
+    else
+	# This lowercasing ${1,,} means BASH 4.0 or greater is required
+	local account=${1,,}
+	local canary=${basedir}/${account}.access_key
+
+	if [[ ! -f "${canary}" ]]; then
+	    echo "Didn't find canary ${canary} for account ${account}"
+	    # exiting here would exit the parent shell
+	else
+	    local filebase=${basedir}/
+
+	    # See if there are cached temporary credentials
+	    local cached=
+	    if _ec2checkcache ${basedir} ${account}; then
+		cached=true
+	    fi
+
+	    if [[ -n "${cached}" ]]; then
+		filebase=${filebase}.cache.
+	    fi
+	
+	    filebase=${filebase}${account}.
+
+	    local access
+	    access=$(cat ${filebase}access_key)
+	    local secret
+	    secret=$(cat ${filebase}secret_key)
+
+	    # Ensure AWS_SECURITY_TOKEN is cleared if not valid
+	    local token=''
+	    if [[ -n "${cached}" ]] && [[ -f "${filebase}session_token" ]]; then
+		token=$(cat ${filebase}session_token)
+	    fi
+
+	    _ec2setvars ${account} ${access} ${secret} ${token}
+
+	    if [[ -n "${cached}" ]]; then
+		export MY_AWS_SET_FOR="$1-MFA-STS"
+	    else
+		export MY_AWS_SET_FOR=$1
+	    fi
+
+	    echo "Set keys for ${MY_AWS_SET_FOR} AWS account"
+
+	    # aws-cli specific environment variable
+	    if [[ -z "${AWS_DEFAULT_REGION}" ]]; then
+		export AWS_DEFAULT_REGION='us-east-1'
+	    fi
+	fi
+    fi
+}
+
+export -f ec2switch
+
+####
+# Show state of AWS-related environment variables
+#
+# Globals: none
+# Arguments: none
+# Returns: none
+####
+function ec2now()
+{
+    if [[ -z "${MY_AWS_SET_FOR}" ]]; then
+	echo "No keys set"
+    else
+	echo "Keys set for ${MY_AWS_SET_FOR} AWS account"
+    fi
+
+    if [[ -n "${AWS_DEFAULT_REGION}" ]]; then
+	echo "Region is ${AWS_DEFAULT_REGION}"
+    fi
+}
+
+export -f ec2now
+
+####
+# Attempt to acquire temporary STS credentials in the currently selected
+# AWS account, then set environment variables based on the newly cached
+# credentials
+#
+# Globals: none
+# Arguments: none
+# Returns: none
+####
+function ec2mfa()
+{
+    if [[ -z "${MY_AWS_SET_FOR}" ]]; then
+	echo "No keys set"
+    elif [[ ${MY_AWS_SET_FOR} =~ MFA-STS ]]; then
+	echo "MFA STS token already cached"
+    else
+	read -e -p "Enter token for user ${USER}: " token
+	# Create credentials good for two hours (7200 minutes)
+	generate-sts-mfa-token --account ${MY_AWS_SET_FOR} --user=${USER} --token=${token} --duration 7200 --cachefiles
+	if [ $? -ne 0 ]; then
+	    echo "Failed to set MFA token"
+	else
+	    ec2switch ${MY_AWS_SET_FOR}
+	fi
+    fi
+}
+
+export -f ec2mfa
